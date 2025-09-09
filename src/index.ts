534d1d0eead19e9a0573f8fcb69f064e06bb6e28
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-09-09 16:48:51 +0100

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {createContract} from "./core/contract";
+export {createMiddleware} from "./core/middleware";
 export {registerContracts} from "./express/register";
 export {generateOpenApi} from "./openapi/generator";
