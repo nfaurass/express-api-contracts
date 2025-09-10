95d20aa8b6cf69b7b25a2c472fcf19035192e1bc
Nidal Faurass <36932043+nfaurass@users.noreply.github.com>
2025-09-10 14:05:13 +0100

    Update request and response types in contract

--- src/core/contract.ts
+++ src/core/contract.ts
@@ -1,14 +1,14 @@
-import {ZodType, infer as ZInfer, ZodObject} from "zod";
+import {ZodObject, ZodType, ZodArray, infer as ZInfer} from "zod";
 import {MiddlewareContract} from "./middleware";
 import {Request, Response} from "express";
 
 // Request Types
 export type RequestMethod = "post" | "put" | "patch" | "delete" | "get";
-type BodyType<BODY extends ZodType | undefined> = BODY extends ZodType ? ZInfer<BODY> : undefined;
-type HeadersType<HEADERS extends ZodType | undefined> = HEADERS extends ZodType ? ZInfer<HEADERS> : undefined;
-type QueryType<QUERY extends ZodType | undefined> = QUERY extends ZodType ? ZInfer<QUERY> : undefined;
-type ParamsType<PARAMS extends ZodType | undefined> = PARAMS extends ZodType ? ZInfer<PARAMS> : undefined;
-type RequestSchema<BODY extends ZodType, HEADERS extends ZodType, QUERY extends ZodType, PARAMS extends ZodType> = {
+type BodyType<BODY extends ZodObject<any> | undefined> = BODY extends ZodObject<any> ? ZInfer<BODY> : undefined;
+type HeadersType<HEADERS extends ZodObject<any> | undefined> = HEADERS extends ZodObject<any> ? ZInfer<HEADERS> : undefined;
+type QueryType<QUERY extends ZodObject<any> | undefined> = QUERY extends ZodObject<any> ? ZInfer<QUERY> : undefined;
+type ParamsType<PARAMS extends ZodObject<any> | undefined> = PARAMS extends ZodObject<any> ? ZInfer<PARAMS> : undefined;
+type RequestSchema<BODY extends ZodObject<any>, HEADERS extends ZodObject<any>, QUERY extends ZodObject<any>, PARAMS extends ZodObject<any>> = {
     body?: BODY;
     headers?: HEADERS;
     query?: QUERY;
@@ -16,13 +16,14 @@ type RequestSchema<BODY extends ZodType, HEADERS extends ZodType, QUERY extends 
 };
 
 // Response Types
-type ResponseSchema<RESPONSE extends { [KEY in number]: ZodType }> = RESPONSE;
+type ResponseSchemaItem = ZodObject<any> | ZodArray<ZodObject<any>>;
+type ResponseSchema<RESPONSE extends { [KEY in number]: ResponseSchemaItem }> = RESPONSE;
 type ResponseUnion<RESPONSE extends ResponseSchema<any>> = {
     [KEY in keyof RESPONSE & number]: { status: KEY; body: ZInfer<RESPONSE[KEY]> };
 }[keyof RESPONSE & number];
 
 // Contract Interface
-export interface Contract<BODY extends ZodType = ZodObject, HEADERS extends ZodType = ZodObject, QUERY extends ZodType = ZodObject, PARAMS extends ZodType = ZodObject, RESPONSE extends Record<number, ZodType> = Record<number, ZodType>> {
+export interface Contract<BODY extends ZodObject<any> = ZodObject<any>, HEADERS extends ZodObject<any> = ZodObject<any>, QUERY extends ZodObject<any> = ZodObject<any>, PARAMS extends ZodObject<any> = ZodObject<any>, RESPONSE extends Record<number, ResponseSchemaItem> = Record<number, ResponseSchemaItem>> {
     name?: string;
     path: string;
     method: RequestMethod;
@@ -32,14 +33,14 @@ export interface Contract<BODY extends ZodType = ZodObject, HEADERS extends ZodT
     handler: (args: {
         req: Request;
         res: Response;
-        body: BodyType<BODY> extends undefined ? undefined : BodyType<BODY>;
-        headers: HeadersType<HEADERS> extends undefined ? undefined : HeadersType<HEADERS>;
-        query: QueryType<QUERY> extends undefined ? undefined : QueryType<QUERY>;
-        params: ParamsType<PARAMS> extends undefined ? undefined : ParamsType<PARAMS>;
+        body: BodyType<BODY>;
+        headers: HeadersType<HEADERS>;
+        query: QueryType<QUERY>;
+        params: ParamsType<PARAMS>;
     }) => Promise<Partial<ResponseUnion<RESPONSE>>> | Partial<ResponseUnion<RESPONSE>>;
 }
 
 // Contract Creator
-export function createContract<BODY extends ZodType = ZodObject, HEADERS extends ZodType = ZodObject, QUERY extends ZodType = ZodObject, PARAMS extends ZodType = ZodObject, RESPONSE extends Record<number, ZodType> = Record<number, ZodType>>(contract: Contract<BODY, HEADERS, QUERY, PARAMS, RESPONSE>) {
+export function createContract<BODY extends ZodObject<any> = ZodObject<any>, HEADERS extends ZodObject<any> = ZodObject<any>, QUERY extends ZodObject<any> = ZodObject<any>, PARAMS extends ZodObject<any> = ZodObject<any>, RESPONSE extends Record<number, ResponseSchemaItem> = Record<number, ResponseSchemaItem>>(contract: Contract<BODY, HEADERS, QUERY, PARAMS, RESPONSE>) {
     return contract;
 }
